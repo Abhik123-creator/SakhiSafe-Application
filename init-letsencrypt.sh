13102b1ac9be4c1e4e9094657b34493080d4d2ac
abhik <54453096+Abhik123-creator@users.noreply.github.com>
2026-06-22 13:16:48 +0530

    Fix entrypoint syntax for docker compose v2 in init script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -21,11 +21,11 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "/bin/sh" certbot -c "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
+    -subj '/CN=localhost'"
 echo
 
 echo "### Starting nginx ..."
@@ -33,10 +33,10 @@ docker compose up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "/bin/sh" certbot -c "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf"
 echo
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
@@ -54,14 +54,13 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+docker compose run --rm certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
-    --force-renewal" certbot
+    --force-renewal
 echo
 
 echo "### Reloading nginx ..."
